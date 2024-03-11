d3d718e93d9bf297590f8d263da157263ff15acd
KhNediem <nedim.khalfallah@medtech.tn>
2024-03-11 22:39:27 +0100

    Added a bunch of new screens

--- mobile/src/navigations/Navigator.js
+++ mobile/src/navigations/Navigator.js
@@ -1,23 +1,24 @@
-import {createStackNavigator} from 'react-navigation-stack'
-import {createAppContainer} from 'react-navigation'
-import Home from '../screens/Home'
-import Cources from '../screens/Cources'
-import Xd from '../screens/Xd'
-import VideoPage from '../screens/VideoPage'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from '../screens/Home';
+import Cources from '../screens/Cources';
+import Xd from '../screens/Xd';
+import VideoPage from '../screens/VideoPage';
 
+const Stack = createNativeStackNavigator();
 
-const stackNavigatorOptions ={
-    headerShown:false
-}
-const AppNavigator = createStackNavigator({
-    Home:{screen:Home},
-    Cources:{screen:Cources},
-    Xd:{screen:Xd},
-    VideoPage:{screen:VideoPage}
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Cources" component={Cources} />
+        <Stack.Screen name="Xd" component={Xd} />
+        <Stack.Screen name="VideoPage" component={VideoPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-},
-{
-    defaultNavigationOptions:stackNavigatorOptions
-}
-)
-export default createAppContainer(AppNavigator);
+export default AppNavigator;
---
+++ mobile/src/screens/index.js
@@ -0,0 +1,15 @@
+import Cources from "./Cources";
+import Home from "./Home";
+import Chapters from "./Chapters";
+import CourseList from "./CourseList";
+import VideoPage from "./VideoPage";
+import Xd from "./Xd";
+
+export {
+    Cources,
+    Home,
+    Chapters,
+    CourseList,
+    VideoPage,
+    Xd
+}
