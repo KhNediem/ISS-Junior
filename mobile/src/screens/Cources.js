97347427e193dec5da1e5320bc3797ff35cafa2f
JEEEEEEEEEEEEEEEEEEEEEED <jed.boulahya@ieee.org>
2024-03-27 14:42:19 +0100

    db fetch sourselist

--- mobile/src/screens/Cources.js
+++ mobile/src/screens/Cources.js
@@ -1,38 +1,57 @@
-import React from 'react';
+import React from "react";
 import {
   View,
   Text,
   Image,
   TouchableOpacity,
   ImageBackground,
-} from 'react-native';
-import { Modalize } from 'react-native-modalize';
-import CourseList from './CourseList';
+} from "react-native";
+const { useState, useEffect } = React;
+import { Modalize } from "react-native-modalize";
+import CourseList from "./CourseList";
 
 const Cources = ({ navigation }) => {
+  const [courseList, setCourseList] = useState([]);
+
+  useEffect(() => {
+    fetchCourseList();
+  }, []);
+
+  const fetchCourseList = async () => {
+    try {
+      const response = await fetch("http://10.0.2.2:3001/languages");
+      const data = await response.json();
+      setCourseList(data);
+    } catch (error) {
+      console.error("Error fetching course list:", error);
+    }
+  };
   return (
     <ImageBackground
-      source={require('../images/cat.png')}
-      style={{ width: '100%', height: '100%' }}
+      source={require("../images/cat.png")}
+      style={{ width: "100%", height: "100%" }}
     >
       <View
         style={{
-          flexDirection: 'row',
-          width: '100%',
+          flexDirection: "row",
+          width: "100%",
           paddingHorizontal: 20,
         }}
       >
         <TouchableOpacity
-          onPress={() => navigation.navigate('Home')}
+          onPress={() => navigation.navigate("Home")}
           style={{
-            paddingHorizontal:10,
-            paddingVertical:13,
-            borderRadius:10, 
-            marginTop:30,
-            backgroundColor:"#8bbcdb"
+            paddingHorizontal: 10,
+            paddingVertical: 13,
+            borderRadius: 10,
+            marginTop: 30,
+            backgroundColor: "#8bbcdb",
           }}
         >
-          <Image source={require('../images/a1.png')} style={{ width: 20, height: 15 }} />
+          <Image
+            source={require("../images/a1.png")}
+            style={{ width: 20, height: 15 }}
+          />
         </TouchableOpacity>
         <View
           style={{
@@ -40,21 +59,24 @@ const Cources = ({ navigation }) => {
             paddingVertical: 13,
             borderRadius: 10,
             marginTop: 30,
-            backgroundColor: '#8bbcdb',
+            backgroundColor: "#8bbcdb",
             marginLeft: 240,
           }}
         >
-          <Image source={require('../images/hum.png')} style={{ height: 15, width: 20 }} />
+          <Image
+            source={require("../images/hum.png")}
+            style={{ height: 15, width: 20 }}
+          />
         </View>
       </View>
       <Text
         style={{
-          color: '#FFF',
+          color: "#FFF",
           fontSize: 35,
-          fontFamily: 'Bold',
+          fontFamily: "Bold",
           width: 200,
-          alignSelf: 'center',
-          textAlign: 'center',
+          alignSelf: "center",
+          textAlign: "center",
           marginTop: 34,
         }}
       >
@@ -64,7 +86,7 @@ const Cources = ({ navigation }) => {
       <Modalize
         handleStyle={{
           marginTop: 30,
-          backgroundColor: '#e9e9e9',
+          backgroundColor: "#e9e9e9",
           width: 80,
         }}
         modalStyle={{
@@ -75,30 +97,15 @@ const Cources = ({ navigation }) => {
         scrollViewProps={{ showsVerticalScrollIndicator: false }}
       >
         <View style={{ marginTop: 40 }}>
-          <CourseList
-            onPress={() => navigation.navigate('Xd')}
-            img={require('../images/xd.png')}
-            title="Adobe XD Prototyping"
-            bg="#fdddf3"
-          />
-          <CourseList img={require('../images/sketch.png')} title="Sketch shortcuts and tricks" bg="#fef8e3" />
-          <CourseList
-            img={require('../images/ae.png')}
-            title="UI Motion Design in After Effects"
-            bg="#fcf2ff"
-          />
-          <CourseList img={require('../images/f.png')} title="Figma Essentials" bg="#fff0ee" />
-          <CourseList
-            img={require('../images/ps.png')}
-            title="Adobe Photoshop. Retouching"
-            bg="#fdddf3"
-          />
-          <CourseList img={require('../images/sketch.png')} title="Sketch shortcuts and tricks" bg="#fef8e3" />
-          <CourseList
-            img={require('../images/ae.png')}
-            title="UI Motion Design in After Effects"
-            bg="#fcf2ff"
-          />
+          {courseList.map((course, index) => (
+            <CourseList
+              key={index}
+              onPress={() => navigation.navigate("Xd")}
+              img={course.img}
+              title={course.LanguageName}
+              bg={course.bg}
+            />
+          ))}
         </View>
       </Modalize>
     </ImageBackground>
